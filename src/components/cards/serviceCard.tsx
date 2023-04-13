213e5003f52925112b46c103840afc44d5b0b8a7
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-04-13 17:12:29 +0300

    service card fix

--- src/components/cards/serviceCard.tsx
+++ src/components/cards/serviceCard.tsx
@@ -17,28 +17,31 @@ export default function ServiceCard({
     description,
     icon,
     image
-} : service) {
+}: service) {
     const cardref = useRef<HTMLDivElement>(null)
     const visible = useIntersection(cardref, "-5px")
-    
-    return(
-        <div 
-        ref={cardref}
-        className={`seviceCard /   / rounded-2xl  max-w-[580px] max-h-[640px] overflow-hidden h-full relative transition-all duration-1000 ease-in-out delay-200 ${visible? "opacity-100 translate-y-0" : "opacity-0 translate-y-1/4"} `}>
-        <img className="h-full w-full  object-cover" src={image} alt="" />
-        <div className="filterLayer / absolute top-0 left-0 h-full w-full bg-mainColor/60 "></div>
-        <div className=" / flex flex-col justify-between / absolute top-0 left-0      h-full w-full p-4 md:p-6">
-            <div className="cardIcon / py-4 px-6 md:py-6 md:px-8 bg-mainColor/40 backdrop-blur-sm w-fit rounded-2xl  ">
-                <MyIcons className={" w-16 h-16 md:w-24 md:h-24"} icon={icon}/>
-            </div>
-            <div className="serviceInfo / flex flex-col gap-2 p-2 / bg-mainColor/40 backdrop-blur-sm rounded-lg ">
-                <div className="w-fit">
-                    <MyText type={"h3"} className={"text-thirdColor font-bold"}>{title}</MyText>
-                    <hr className="border-thirdColor rounded border-[1px] " />
+
+    return (
+        <div
+            ref={cardref}
+            className={`seviceCard /   / rounded-2xl  max-w-[580px] max-h-[640px] overflow-hidden h-full relative transition-all duration-1000 ease-in-out delay-200 ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-1/4"} `}>
+
+            <img className="h-full w-full  object-cover top-0 left-0 absolute" src={image} alt="" />
+
+            <div className="filterLayer / absolute top-0 left-0 h-full w-full bg-mainColor/60 "></div>
+
+            <div className=" / flex flex-col  justify-between gap-20 / min-h-[400px] md:min-h-[640px] h-full  w-full p-3 md:p-6">
+                <div className="cardIcon / py-3 px-5 md:py-6 md:px-8 bg-mainColor/40 backdrop-blur-sm w-fit rounded-2xl  ">
+                    <MyIcons className={" w-16 h-16 md:w-24 md:h-24"} icon={icon} />
                 </div>
-                <MyText type={"p"} className={"text-thirdColor3 "}>{description}</MyText>
+                <div className="serviceInfo / flex flex-col gap-2 p-2 / bg-mainColor/40 backdrop-blur-sm rounded-lg ">
+                    <div className="w-fit">
+                        <MyText type={"h3"} className={"text-thirdColor font-bold"}>{title}</MyText>
+                        <hr className="border-thirdColor rounded border-[1px] " />
+                    </div>
+                    <MyText type={"p"} className={"text-thirdColor3 "}>{description}</MyText>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
