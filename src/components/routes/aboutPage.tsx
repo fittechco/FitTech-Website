f6ed85ee8b07107418b6e42ebbcde35a7377ab11
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-04-14 16:38:26 +0300

    about us title fix

--- src/components/routes/aboutPage.tsx
+++ src/components/routes/aboutPage.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   return (
     <div className="aboutPage / flex flex-col gap-20 / w-full h-full relative">
       <Helmet>
-        <title>About FitTech - Our Mission and Values</title>
+        <title>About FitTech - Our Vision and Mission</title>
         <meta name="description" content="FitTech is a startup agency that specializes in providing technology and services to the fitness industry. Our mission is to help gym businesses and supplement stores succeed with innovative solutions." />
         <meta name="keywords" content="FitTech founders, fitness industry technology, gym business solutions, supplement store services, innovative fitness solutions, fitness technology company" />
       </Helmet>
