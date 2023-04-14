85a4478fc0e14113f7a6e8cd27e51d02f7c9454c
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2023-04-14 16:36:45 +0300

    service card fix

--- src/components/routes/aboutPage.tsx
+++ src/components/routes/aboutPage.tsx
@@ -1,8 +1,14 @@
 import AboutUs from "../sections/aboutSection/aboutUs";
+import { Helmet } from 'react-helmet';
 
 export default function AboutPage() {
   return (
     <div className="aboutPage / flex flex-col gap-20 / w-full h-full relative">
+      <Helmet>
+        <title>About FitTech - Our Mission and Values</title>
+        <meta name="description" content="FitTech is a startup agency that specializes in providing technology and services to the fitness industry. Our mission is to help gym businesses and supplement stores succeed with innovative solutions." />
+        <meta name="keywords" content="FitTech founders, fitness industry technology, gym business solutions, supplement store services, innovative fitness solutions, fitness technology company" />
+      </Helmet>
       <AboutUs />
     </div>
   );
